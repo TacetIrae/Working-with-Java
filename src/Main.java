7eebb048fde6707010a9f085b9fe8bfc082b98f0
Egor <egorbrylev123@gmail.com>
2023-02-04 15:33:30 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int x = 123;
+        int y = 100;
+        System.out.println(x+y);
+    }
+}
